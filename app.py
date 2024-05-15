7baab57725822b0211be2045f2c0a985d46dacf2
Cássio Nhiemetz <cassionhieg@proton.me>
2024-05-15 14:11:03 -0300

    new background and window size

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from gui import mostrar_programas_gui
+from programs import listar_programas_instalados
+
+
 if __name__ == "__main__":
     programas = listar_programas_instalados()
     mostrar_programas_gui(programas)
--- gui.py
+++ gui.py
@@ -14,14 +14,18 @@ def mostrar_programas_gui(programas):
         
         webbrowser.open_new_tab('lista_programas.html')
 
+    icone = 'icone.ico'
+
     root = Tk()
+    root.title("WinProgramLister - by Cassio Nhiemetz")
+    root.iconbitmap(icone)
     frame = Frame(root)
     frame.pack()
 
     scrollbar = Scrollbar(frame)
     scrollbar.pack(side="right", fill="y")
 
-    text_widget = Text(frame, wrap="word", yscrollcommand=scrollbar.set)
+    text_widget = Text(frame, wrap="word", yscrollcommand=scrollbar.set, width=120, height=35, bg="#8d9091")
     text_widget.pack(fill="both", expand=True)
 
     scrollbar["command"] = text_widget.yview
@@ -31,5 +35,7 @@ def mostrar_programas_gui(programas):
 
     button = Button(root, text="Gerar HTML", command=lambda: gerar_html(programas))
     button.pack(pady=10)
+    root.geometry("1000x600")
+    root.configure(bg="#302E2F")
 
     root.mainloop()
